8ffc038024e30abdcf8f024f4ac1571fa9781d74
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-05 21:54:54 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -62,14 +62,13 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-
+      # :yukihiro_matsumoto => {
+      #     :known_for => "Ruby",
+      #     :languages => ["LISP", "C"]
+      #   }
 
 	programmer_hash = 
  		{
- 		    :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
-        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -83,7 +82,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+    programmer_hash[:yukihiro_matsumoto] = {}
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
     programmer_hash
 end
 
@@ -105,8 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for] = "GUI"
-     programmer_hash[:alan_kay][:known_for]
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash
 
 end
